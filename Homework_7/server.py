6bbdebb3d70c0004ab8340706995ab4f460037ff
Aleksandra0001 <104300297+Aleksandra0001@users.noreply.github.com>
2022-08-22 16:33:10 +0300

    chat final version

--- Homework_7/server.py
+++ Homework_7/server.py
@@ -1,44 +1,52 @@
 import socket
 from threading import Thread
 
-HOSTNAME = socket.gethostname()
-SERVER_IP = socket.gethostbyname(HOSTNAME)
-SERVER_PORT = 5005
-separator_token = '<SEP> '
-
-s = socket.socket()
-client_sockets = set()
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5002
+separator_token = "<SEP>"
 
 
-def handle_client(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            message = message.replace(separator_token, ': ')
-            print(f"[*]-{client_socket.getpeername()} Received message: {message}")
-            for client_socket in client_sockets:
-                client_socket.send(message.encode())
-        except Exception as e:
-            print(f"[-] {client_socket.getpeername()} is disconnected. - {e}")
-            client_sockets.remove(client_socket)
-            break
+def start_server():
+    client_sockets = set()
 
+    def listen_for_client(client_socket: socket.socket):
+        while True:
+            try:
+                msg = client_socket.recv(1024).decode()
+                if not msg:
+                    print(f"[-] Client {client_socket.getpeername()} is offline")
+                    client_sockets.remove(client_socket)
+                    break
+                else:
+                    msg = msg.replace(separator_token, ": ")
+                    print(f"[+] Received message from {client_socket.getpeername()}: {msg}")
 
-def run_server():
+                for cs in client_sockets:
+                    cs.send(msg.encode())
+
+            except Exception as e:
+                print(f"[!] Error: {e}")
+                client_sockets.remove(client_socket)
+
+    s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((SERVER_IP, SERVER_PORT))
-    s.listen(10)
-    print(f"[*] Listening as {HOSTNAME}:{SERVER_PORT}")
-    while True:
-        client_socket, client_address = s.accept()
-        print(f"[*] Connection from {client_address}")
-        client_sockets.add(client_socket)
-        t = Thread(target=handle_client, args=(client_socket,))
-        t.start()
+    s.bind((SERVER_HOST, SERVER_PORT))
+    s.listen(5)
+    print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+    try:
+        while True:
+            client_socket, client_address = s.accept()
+            print(f"[+] {client_address} connected.")
+            client_sockets.add(client_socket)
+            t = Thread(target=listen_for_client, args=(client_socket,), daemon=True)
+            t.start()
+    except Exception as e:
+        print(f"[!] Server error: {e}")
+        for cs in client_sockets:
+            cs.close()
+        s.close()
 
 
-if __name__ == '__main__':
-    run_server()
-    for cs in client_sockets:
-        cs.close()
-    s.close()
+if __name__ == "__main__":
+    start_server()
