44aa6e3ed19ecad2d01d662f3f1d20b78270622e
Aleksandra0001 <104300297+Aleksandra0001@users.noreply.github.com>
2022-09-08 19:58:47 +0300

    added classes in models.py

--- Homework_9/models.py
+++ Homework_9/models.py
@@ -6,37 +6,26 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey, Table
 from sqlalchemy.sql.sqltypes import DateTime
 
-
 Base = declarative_base()
 
-# таблица для связи many-to-many между таблицами notes и tags
-note_m2m_tag = Table(
-    "note_m2m_tag",
-    Base.metadata,
-    Column("id", Integer, primary_key=True),
-    Column("note", Integer, ForeignKey("notes.id")),
-    Column("tag", Integer, ForeignKey("tags.id")),
-)
 
-# Таблица notes, где будут храниться названия списка дел
-class Note(Base):
-    __tablename__ = "notes"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    created = Column(DateTime, default=datetime.now())
-    records = relationship("Record", cascade="all, delete", backref="note")
-    tags = relationship("Tag", secondary=note_m2m_tag, backref="notes")
+@property
+def full_name(self):
+    return f'{self.name} {self.surname}'
 
-# Таблица records, где будут храниться записи для конкретного дела из таблицы notes - связь one-to-one, поле note_id
-class Record(Base):
-    __tablename__ = "records"
-    id = Column(Integer, primary_key=True)
-    description = Column(String(150), nullable=False)
-    done = Column(Boolean, default=False)
-    note_id = Column(Integer, ForeignKey(Note.id, ondelete="CASCADE"))
 
-# Таблица tags, где хранится набор тегов для списка дел.
-class Tag(Base):
-    __tablename__ = "tags"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(25), nullable=False, unique=True)
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column('id', Integer, primary_key=True)
+    email = Column('email', String(50), nullable=False)
+    phone = Column('phone_number', String(50), nullable=False)
+    birthday = Column('birthday', DateTime, nullable=False)
+    address = Column('address', String(50), nullable=False)
+
+
+class Notebook_record(Base):
+    __tablename__ = "notebook_record"
+    id = Column('id', Integer, primary_key=True)
+    notebook_title = Column('notebook_title', String(50), nullable=False)
+    notebook_text = Column('notebook_text', String(500), nullable=False)
+    notebook_tag = Column('notebook_tag', String(50), nullable=False)
