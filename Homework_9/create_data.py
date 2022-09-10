e54bde5ce20ccbcb0b6593f34bf93ce9687f6818
Aleksandra0001 <104300297+Aleksandra0001@users.noreply.github.com>
2022-09-10 20:49:48 +0300

    Finish

--- Homework_9/create_data.py
+++ Homework_9/create_data.py
@@ -6,10 +6,10 @@ from models import (
     Contact, Phone, Email,
 )
 
-fake = Faker()
+fake = Faker('uk_UA')
 
 
-def create_contacts():
+def generate_fake_contacts():
     for i in range(100):
         contact_first_name = fake.first_name()
         contact_last_name = fake.last_name()
@@ -32,3 +32,55 @@ def create_contacts():
         session.add(phone)
         session.add(email)
         session.commit()
+
+
+def add_phone(contact_id, phone):
+    contact = session.query(Contact).filter(Contact.contact_id == contact_id).first()
+    phone = Phone(
+        phone=phone,
+        contact=contact,
+    )
+    session.add(phone)
+    session.commit()
+
+
+def add_email(contact_id, email):
+    contact = session.query(Contact).filter(Contact.contact_id == contact_id).first()
+    email = Email(
+        email=email,
+        contact=contact,
+    )
+    session.add(email)
+    session.commit()
+
+
+def add_contact(first_name, last_name, phone, email):
+    contact = Contact(
+        first_name=first_name,
+        last_name=last_name,
+    )
+    phone = Phone(
+        phone=phone,
+        contact=contact,
+    )
+    email = Email(
+        email=email,
+        contact=contact,
+    )
+    session.add(contact)
+    session.add(phone)
+    session.add(email)
+    session.commit()
+
+
+def get_contact_by_id(contact_id):
+    contact = session.query(Contact).filter(Contact.contact_id == contact_id).first()
+    return contact
+
+
+def delete_contact(contact_id):
+    contact = session.query(Contact).filter(Contact.contact_id == contact_id).first()
+    session.delete(contact)
+    session.commit()
+
+
--- Homework_9/models.py
+++ Homework_9/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import (
     Column,
     Integer, String, Date,
     DateTime,
-    Table,
+    Table,Identity
 )
 from sqlalchemy.orm import relationship
 
@@ -13,10 +13,10 @@ from db import Base, metadata
 class Contact(Base):
     __tablename__ = 'contacts'
     contact_id = Column('contact_id', Integer, primary_key=True)
-    first_name = Column('first_name', String(50), nullable=True)
-    last_name = Column('last_name', String(50), nullable=True)
-    phone = relationship("Phone", back_populates="contact")
-    email = relationship("Email", back_populates="contact")
+    first_name = Column('first_name', String(50), nullable=False)
+    last_name = Column('last_name', String(50), nullable=False)
+    phone = relationship("Phone", back_populates="contact", cascade="all, delete-orphan")
+    email = relationship("Email", back_populates="contact", cascade="all, delete-orphan")
 
     @property
     def full_name(self):
@@ -26,14 +26,14 @@ class Contact(Base):
 class Phone(Base):
     __tablename__ = 'phones'
     phone_id = Column('phone_id', Integer, primary_key=True)
-    phone = Column('phone', String(50), nullable=True)
-    contact_id = Column('contact_id', Integer, ForeignKey('contacts.contact_id'))
+    phone = Column('phone', String(50), nullable=True,default='No phone')
+    contact_id = Column('contact_id', Integer, ForeignKey('contacts.contact_id'), nullable=False)
     contact = relationship("Contact", back_populates="phone")
 
 
 class Email(Base):
     __tablename__ = 'emails'
     email_id = Column('email_id', Integer, primary_key=True)
-    email = Column('email', String(50), nullable=True)
-    contact_id = Column('contact_id', Integer, ForeignKey('contacts.contact_id'))
+    email = Column('email', String(50), nullable=True,default='No email')
+    contact_id = Column('contact_id', Integer, ForeignKey('contacts.contact_id'), nullable=False)
     contact = relationship("Contact", back_populates="email")
